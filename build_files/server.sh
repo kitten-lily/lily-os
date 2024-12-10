eb28d18d6330eb94d85a75fba6edf8be4b7b689d
Lily <20535390+kitten-lily@users.noreply.github.com>
2024-12-10 14:43:31 +0100

    Update server.sh

--- build_files/server.sh
+++ build_files/server.sh
@@ -6,5 +6,3 @@ set -euox pipefail
 /ctx/build_files/server/repos.sh
 /ctx/build_files/server/packages.sh
 /ctx/build_files/server/cleanup.sh
-
-rpm-ostree initramfs --enable --arg=--force-add --arg=tpm2-tss
