376a84e88e7a12133825ec15797cf7929460c6af
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-01-02 22:52:32 +0100

    Add incus

--- build_files/ccos/packages.sh
+++ build_files/ccos/packages.sh
@@ -2,6 +2,25 @@
 
 set -euox pipefail
 
+# Incus
+#     edk2-ovmf
+#     genisoimage
+#     incus-agent
+#     incus-client
+#     qemu-char-spice
+#     qemu-device-display-virtio-vga
+#     qemu-device-display-virtio-gpu
+#     qemu-device-usb-redirect
+#     qemu-img
+#     qemu-kvm-core
+#     swtpm
+
+dnf -y install \
+    incus \
+    qemu-img \
+    qemu-kvm-core
+
+
 # Tailscale
 dnf -y --enablerepo tailscale-stable install \
   tailscale
--- build_files/ccos/repos.sh
+++ build_files/ccos/repos.sh
@@ -2,6 +2,12 @@
 
 set -euox pipefail
 
+# EPEL
+dnf -y install epel-release epel-next-release
+
+# Incus
+dnf -y copr enable neil/incus
+
 # Tailscale
 dnf config-manager --add-repo https://pkgs.tailscale.com/stable/centos/9/tailscale.repo
 dnf config-manager --set-disabled tailscale-stable
