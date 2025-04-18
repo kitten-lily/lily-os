44a12e7a8715d9a382b63cfb72593214ec2e2cf9
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-04-18 12:48:18 +0200

    Disable docker

--- build_files/05-services.sh
+++ build_files/05-services.sh
@@ -4,6 +4,8 @@ set -ouex pipefail
 
 sudo systemctl enable coolercontrold
 
+systemctl disable docker.socket
+systemctl disable docker.service
 systemctl enable incus.socket
 systemctl enable incus.service
 systemctl enable incus-startup
