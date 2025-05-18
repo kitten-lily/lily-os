d5521f422c20712e803e106c477f8ccc59fd35d8
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-05-18 08:40:17 +0200

    Remove mkosi

--- build_files/04-install-packages.sh
+++ build_files/04-install-packages.sh
@@ -4,9 +4,6 @@ mkdir /var/roothome
 
 set -ouex pipefail
 
-dnf -y install --setopt=install_weak_deps=False \
-   mkosi
-
 # Cooler Control
 dnf -y copr enable codifryed/CoolerControl
 dnf -y install \
